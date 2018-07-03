dd9456febc191fb1de56a795217a603623c0537c
Leonard Soai-Van <leo.soaivan@gmail.com>
2018-07-03 16:38:43 -0400

    Add event listener to squares

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const gameBoard = (() => {
-  let contents = Array(9).fill('X');
+  let contents = Array(9).fill('');
+
   return {
     contents,
   };
@@ -11,19 +12,44 @@ const displayController = (() => {
   const displayGameBoardContents = () => {
     let contents = gameBoard.contents;
 
-    for (let i = 0; i < gameBoardContents.length; i++) {
+    for (let i = 0; i < contents.length; i++) {
       let boardSquare = document.querySelector(`[data-board-index="${i}"]`);
 
-      boardSquare.innerHTML = gameBoardContents[i];
+      boardSquare.innerHTML = contents[i];
     }
   };
+
   return {
     displayGameBoardContents,
   };
 })();
 
-const PlayerFactory = (symbol) => {
+const playerFactory = (symbol) => {
   return {symbol};
 };
 
-displayController.displayGameBoardContents();
+const gameFactory = () => {
+  const gameBoardSquares = [...document.querySelectorAll('.square')];
+
+  const loadSquares = () => {
+    gameBoardSquares.forEach((square) => {
+      square.onclick = () => {
+        _modifySquare(square);
+      };
+    });
+  };
+  
+  const _modifySquare = (square) => {
+    let indexToModify = square.getAttribute('data-board-index');
+    
+    gameBoard.contents[indexToModify] = 'x';
+    displayController.displayGameBoardContents();
+  };
+
+  return {
+    loadSquares,
+  };
+};
+
+let game = gameFactory();
+game.loadSquares();
